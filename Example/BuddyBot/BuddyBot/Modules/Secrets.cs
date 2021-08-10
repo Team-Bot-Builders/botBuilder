0290310680cb8e52af8f02381b5db0a514bd497f
Chaz_B <chaboffe@gmail.com>
2021-08-09 18:12:03 -0700

    Taking out the token to appsettings.json

---
+++ Example/BuddyBot/BuddyBot/Modules/Secrets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuddyBot.Modules
+{
+    public class Secrets
+    {
+        public const string SecretToken = "SecretToken";
+        public string BotToken { get; set; }
+
+    }
+}
